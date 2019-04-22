381125d2826d6e73bc8ef4b06f78aae3c4792e12
Manuel Fernandez <manferlo81@gmail.com>
2019-04-22 15:06:00 +0700

    implement build

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,14 @@
 import buble from "rollup-plugin-buble";
 import optimize from "./plugins/optimize";
+import dts from "rollup-plugin-dts";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
 
-import { main, module as esModule, dependencies } from "./package.json";
+import { main, module as esModule, typings, dependencies } from "./package.json";
 
 const DEV = process.env.ROLLUP_WATCH;
 
-const input = "src/index.js";
+const input = "src/index.ts";
 const sourcemap = true;
 
 /** @type {import("rollup").OutputOptions} */
@@ -23,17 +26,71 @@ const esOutput = {
   sourcemap,
 };
 
+const dtsOutput = {
+  file: typings,
+  format: "es",
+};
+
 const external = Object.keys(dependencies);
 
-/** @type {import("rollup").RollupOptions} */
-const config = {
+// /** @type {import("rollup").RollupOptions} */
+// const config = {
 
-  input,
+//   input,
+//   output: [cjsOutput, esOutput],
+
+//   external,
+
+//   plugins: [
+
+//     buble({
+//       target: {
+//         node: 0.12,
+//         ie: 8,
+//         chrome: 48,
+//         firefox: 43,
+//         safari: 8,
+//         edge: 12,
+//       },
+//     }),
+
+//     !DEV && optimize({
+//       sourcemap,
+//       toplevel: true,
+//       nameCache: {},
+//       indent: 2,
+//       quote: "\"",
+//     }),
+
+//   ],
+
+// };
+
+
+/** @type {import("rollup").RollupOptions} */
+const jsConfig = {
+
+  input: "src/index.ts",
   output: [cjsOutput, esOutput],
 
   external,
 
   plugins: [
+
+    resolve({
+      jsnext: true,
+      extensions: [".ts"],
+    }),
+
+    commonjs(),
+
+    dts({
+      banner: !!DEV,
+      compileMode: "js",
+      compilerOptions: {
+        target: "esnext",
+      },
+    }),
 
     buble({
       target: {
@@ -58,4 +115,23 @@ const config = {
 
 };
 
-export default config;
+/** @type {import("rollup").RollupOptions} */
+const dtsConfig = {
+
+  input,
+  output: dtsOutput,
+
+  external,
+
+  plugins: [
+
+    dts({
+      banner: !!DEV,
+      compileMode: "dts",
+    }),
+
+  ],
+
+};
+
+export default [jsConfig, dtsConfig];
