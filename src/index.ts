c0cebc1098e35577ac2b58f3838a4ec17409eb9a
Manuel Fernandez <manferlo81@gmail.com>
2019-11-07 17:47:43 +0700

    changing code style

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,39 @@
-import isArrayLike from "is-array-like";
+import isArrayLike from 'is-array-like'
 
 function toArray<T>(
   args: IArguments | T[] | string | ArrayLike<T>,
   start?: number | null,
 ): T[] {
 
-  if (!isArrayLike(args) && args !== "") {
-    throw new TypeError(`${args} can't be converted to array.`);
+  if (!isArrayLike(args) && args !== '') {
+    throw new TypeError(`${args} can't be converted to array.`)
   }
 
   if (start == null) {
-    start = 0;
+    start = 0
   }
 
-  if (typeof start !== "number" || !isFinite(start)) {
-    throw new TypeError(`${start} is not a valid start point.`);
+  if (typeof start !== 'number' || !isFinite(start)) {
+    throw new TypeError(`${start} is not a valid start point.`)
   }
 
-  const len = args.length;
+  const len = args.length
 
   if (start < 0) {
-    start += len;
+    start += len
   }
 
-  const argsObj = Object(args);
-  const result = new Array(len - start);
+  const argsObj = Object(args)
+  const result = new Array(len - start)
 
   for (let i = start; i < len; i++) {
     if (i in argsObj) {
-      result[i - start] = argsObj[i];
+      result[i - start] = argsObj[i]
     }
   }
 
-  return result;
+  return result
 
 }
 
-export default toArray;
+export default toArray
