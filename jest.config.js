16f205701394e938fffe532842f95070839cba36
Manuel Fernandez <manferlo81@gmail.com>
2020-03-21 22:03:11 +0700

    lint jest config

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,23 @@
-const minCoverage = 95;
+const minCoverage = 95
 
 module.exports = {
 
-  testEnvironment: "node",
+  testEnvironment: 'node',
   browser: false,
 
-  cacheDirectory: "node_modules/.cache/jest",
+  cacheDirectory: 'node_modules/.cache/jest',
 
-  preset: "ts-jest",
+  preset: 'ts-jest',
 
   collectCoverage: true,
   collectCoverageFrom: [
-    "src/**/*.ts",
+    'src/**/*.ts',
   ],
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
   coverageReporters: [
-    process.env.CI ? "json" : "lcov",
-    "text",
-    "text-summary",
+    process.env.CI ? 'json' : 'lcov',
+    'text',
+    'text-summary',
   ],
   coverageThreshold: {
     global: {
@@ -30,4 +30,4 @@ module.exports = {
 
   verbose: null,
 
-};
+}
