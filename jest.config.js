a2070ac692f1bbc20cafadffe32ede4bbcab24ae
Manuel Fernandez <manferlo81@gmail.com>
2020-05-18 23:31:04 +0700

    chore: Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,8 @@
 module.exports = {
 
   testEnvironment: 'node',
-
   cacheDirectory: 'node_modules/.cache/jest',
-
   preset: 'ts-jest',
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.json',
-    },
-  },
 
   collectCoverage: true,
   collectCoverageFrom: [
@@ -22,6 +15,6 @@ module.exports = {
     'text-summary',
   ],
 
-  verbose: null,
+  verbose: true,
 
 }
