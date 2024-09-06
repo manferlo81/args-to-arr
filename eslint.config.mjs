6731d547e62c809ebe33ff0c20c441bb29b6b814
Manuel Fernández <manferlo81@gmail.com>
2024-09-06 17:42:48 +0700

    chore: Install and setup ESLint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,64 @@
+import pluginJs from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import globals from 'globals';
+import { config, configs as typescriptConfigs } from 'typescript-eslint';
+
+function normalizeRuleEntry(entry) {
+  if (Array.isArray(entry)) return entry;
+  if (['error', 'warn', 'off'].includes(entry)) return entry;
+  return ['error', entry];
+}
+
+function createRuleNameNormalizer(pluginName) {
+  if (!pluginName) return (ruleName) => ruleName;
+  return (ruleName) => {
+    const pluginPrefix = `${pluginName}/`;
+    if (ruleName.startsWith(pluginPrefix)) return ruleName;
+    return `${pluginPrefix}${ruleName}`;
+  };
+}
+
+function normalizeRules(pluginName, rules) {
+  const normalizeRuleName = createRuleNameNormalizer(pluginName);
+  return Object.fromEntries(
+    Object.entries(rules).map(([ruleName, ruleEntry]) => {
+      return [normalizeRuleName(ruleName), normalizeRuleEntry(ruleEntry)];
+    }),
+  );
+}
+
+const eslintRules = normalizeRules(null, {
+  'no-useless-rename': 'error',
+  'object-shorthand': 'error',
+});
+
+const stylisticRules = normalizeRules('@stylistic', {
+  semi: 'always',
+  indent: 2,
+  quotes: 'single',
+  'linebreak-style': 'windows',
+
+  'quote-props': 'as-needed',
+  'arrow-parens': 'always',
+  'no-extra-parens': 'all',
+  'no-extra-semi': 'error',
+
+  'padded-blocks': 'off',
+});
+
+const typescriptRules = normalizeRules('@typescript-eslint', {
+  'no-explicit-any': 'off',
+});
+
+const typescriptConfig = config(
+  ...typescriptConfigs.recommended,
+);
+
+export default config(
+  { ignores: ['dist', 'coverage'] },
+  { languageOptions: { globals: { ...globals.node, ...globals.browser } } },
+  pluginJs.configs.recommended,
+  stylistic.configs['recommended-flat'],
+  ...typescriptConfig,
+  { rules: { ...eslintRules, ...stylisticRules, ...typescriptRules } },
+);
