33a275699b57f08cef609d7892010b36c08ed0b7
Manuel Fernández <manferlo81@gmail.com>
2024-09-09 07:51:23 +0700

    chore: Create minified ES Module

--- bundlib.config.mjs
+++ bundlib.config.mjs
@@ -2,7 +2,7 @@ import { config } from 'bundlib';
 
 export default config({
   name: 'toArray',
-  min: 'browser',
+  min: ['browser', 'module'],
   equals: true,
   project: 'tsconfig-build.json',
 });
